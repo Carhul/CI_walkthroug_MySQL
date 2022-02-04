2315102245014367dbe784d541174b89ac57d55a
Carhul <carolinahulloen@gmail.com>
2022-02-04 17:34:21 +0000

    Add mysql file

--- mysql/mysql-from-python.py
+++ mysql/mysql-from-python.py
@@ -19,5 +19,6 @@ try:
         result = cursor.fetchall()
         print(result)
 finally:
-    # Close the connection, regardless of whether or not the above was successful
+    # Close the connection
+    # Regardless of whether or not the above was successful
     connection.close()
