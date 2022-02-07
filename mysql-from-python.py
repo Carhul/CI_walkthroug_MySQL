276b49e2f2a8a270532791b2abed17d1211f1867
Carhul <carolinahulloen@gmail.com>
2022-02-07 11:13:21 +0000

    Change username=user to username=root

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,14 +1,14 @@
 import os
 import pymysql
 
-# Get username from Cloud9 workspace
+# Get username from Gitpod Workspace
 # (modify this variable if running on another environment)
-username = os.getenv('C9_USER')
+username = os.getenv('carhul')
 
 # Connect to the database
 connection = pymysql.connect(host='localhost',
-                             user=username,
-                             password='',
+                             user='root',
+                             password="",
                              db='Chinook')
 
 try:
